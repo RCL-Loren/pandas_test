b8894634bd41f4c48512b99d60fb0463a02a2ae6
Loren Charnley <loren@resonantcircuitlabs.com>
2020-12-01 23:09:32 -0500

    Added functionality with two passing unit tests

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,4 +1,13 @@
 import pandas as pd
 
-def load_testing_data(data_filename):
-	return data_filename
+def load_testing_data(data_filename , headings):
+	df = pd.read_csv(data_filename,
+	                             encoding = "utf16",
+	                             sep='\t',
+	                             thousands=',',
+	                             index_col='Date',
+	                             parse_dates=True,
+	                             ).sort_index(ascending=True, axis=0)
+	df.columns = headings
+
+	return df
--- analysis/test_analysis.py
+++ analysis/test_analysis.py
@@ -1,4 +1,14 @@
 from pandas_test.analysis.analysis import load_testing_data
+import pandas as pd
 
-def test_load_testing_data():
-	assert load_testing_data("./test_data/test_data_dailytest.csv") == "./test_data/test_data_dailytest.csv"
+in_file = "~/src/pandas_test/analysis/test_data/test_data_dailytests.csv"
+headers = ["Total", "PCR_Tests", "Antigen_Tests", "Pos_Percent"]
+
+def test_load_testing_data_shape():
+	df = load_testing_data(in_file,headers)
+	assert df.shape == (3,4)
+
+def test_load_testing_data_columns():
+	df = load_testing_data(in_file,headers)
+	tmp = list(df.columns)
+	assert tmp == headers
