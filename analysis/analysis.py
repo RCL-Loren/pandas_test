b0e81f4d93f89ffb2364385bb2dde6074a6dce02
Loren Charnley <loren@resonantcircuitlabs.com>
2020-12-01 23:17:48 -0500

    Added 2 more CSV files as fixed test files

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def load_testing_data(data_filename , headings):
+def load_ncdhhs_data(data_filename , headings):
+	#NC DHHS has data set to download as UTF16 LE BOM
 	df = pd.read_csv(data_filename,
 	                             encoding = "utf16",
 	                             sep='\t',
@@ -9,5 +10,5 @@ def load_testing_data(data_filename , headings):
 	                             parse_dates=True,
 	                             ).sort_index(ascending=True, axis=0)
 	df.columns = headings
-
 	return df
+
--- analysis/test_analysis.py
+++ analysis/test_analysis.py
@@ -1,14 +1,14 @@
-from pandas_test.analysis.analysis import load_testing_data
+from pandas_test.analysis.analysis import load_ncdhhs_data
 import pandas as pd
 
-in_file = "~/src/pandas_test/analysis/test_data/test_data_dailytests.csv"
+in_file = "~/src/pandas_test/analysis/test_data/TABLE_DAILY_TESTING_METRICS.csv"
 headers = ["Total", "PCR_Tests", "Antigen_Tests", "Pos_Percent"]
 
 def test_load_testing_data_shape():
-	df = load_testing_data(in_file,headers)
-	assert df.shape == (3,4)
+	df = load_ncdhhs_data(in_file,headers)
+	assert df.shape == (275,4)
 
 def test_load_testing_data_columns():
-	df = load_testing_data(in_file,headers)
+	df = load_ncdhhs_data(in_file,headers)
 	tmp = list(df.columns)
 	assert tmp == headers
