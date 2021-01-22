8a2b5f6f023fa601f823d0e79d40fc6b65d043fa
lc3 <loren@resonantcircuitlabs.com>
2021-01-22 10:19:26 -0500

    Added a function to aggregate daily observations to weekly on a single column

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -15,3 +15,17 @@ def load_ncdhhs_data(data_filename , headings=[]):
 
 	return df
 
+def aggregate_weekly(daily_testing_df, column_name):
+	"""Extract a column, resample to weekly, and add an integer reference"""
+
+	# Create new dataframe with only Total Tests and drop rows missing values
+	df_test_vol = daily_testing_df[[column_name]]
+	df_test_vol = df_test_vol.dropna(axis=0, how='any')
+
+	# Resample and aggregate to weekly
+	df_test_vol = df_test_vol.resample('W-MON').sum()
+
+	# Create a sequence of integers to reference as week from start
+	df_test_vol['Week'] = range(1,len(df_test_vol) + 1)
+
+	return df_test_vol[:-1]
--- analysis/test_analysis.py
+++ analysis/test_analysis.py
@@ -1,15 +1,16 @@
 from pandas_test.analysis.analysis import load_ncdhhs_data
+from pandas_test.analysis.analysis import aggregate_weekly
 import pandas as pd
 
-in_file1 = "~/src/pandas_test/analysis/test_data/TABLE_DAILY_TESTING_METRICS.csv"
+in_file1 = "./analysis/test_data/TABLE_DAILY_TESTING_METRICS.csv"
 headers1 = ["Total", "PCR_Tests", "Antigen_Tests", "Pos_Percent"]
 
-in_file2 = "~/src/pandas_test/analysis/test_data//TABLE_DAILY_CASE&DEATHS_METRICS.csv"
+in_file2 = "./analysis/test_data//TABLE_DAILY_CASE&DEATHS_METRICS.csv"
 headers2 = ["Cases", "PCR", "Antigen", "Deaths"]
 
 def test_load_testing_data_shape_testing():
 	df = load_ncdhhs_data(in_file1,headers1)
-	assert df.shape == (275,4)
+	assert df.shape == (326,4)
 
 def test_load_testing_data_columns_testing():
 	df = load_ncdhhs_data(in_file1,headers1)
@@ -24,3 +25,9 @@ def test_load_testing_data_columns_cases():
 	df = load_ncdhhs_data(in_file2, headers2)
 	tmp = list(df.columns)
 	assert tmp == headers2
+
+def test_aggregate_weekly():
+	df = load_ncdhhs_data(in_file1, headers1)
+	df1 = aggregate_weekly(df, 'Total')
+	diff = df1.index[1] - df1.index[0]
+	assert diff.days == 7
